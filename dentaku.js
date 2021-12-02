de15969b35326fad30d8cfdccad9596c8b3cdd08
EC2 Default User <ec2-user@ip-172-31-62-233.ec2.internal>
2021-12-02 13:14:57 +0000

    提出

---
+++ dentaku.js
@@ -0,0 +1,10 @@
+var result = document.getElementById("result");
+    function edit(elem) {
+      result.value = result.value + elem.value;
+    }
+    function calc() {
+      result.value = new Function("return " + result.value)();
+    }
+    function reset() {
+      document.number.reset();
+    }
